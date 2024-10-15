82e51f3cd7384f73be30d05d735a00243bf41317
phy122 <qkrghdud369@gmail.com>
2024-10-15 18:10:30 +0900

    2024-10-15 Servlet Study

---
+++ HTTP/src/main/java/Board/domain/Board.java
@@ -0,0 +1,67 @@
+package Board.domain;
+
+public class Board {
+	private String title;
+	private String writer;
+	private String content;
+	private int no;
+	
+	public Board() {
+		
+	}
+
+	
+
+	public Board(String title, String writer, String content) {
+		this.title = title;
+		this.writer = writer;
+		this.content = content;
+	}
+
+
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	@Override
+	public String toString() {
+		return "Board [title=" + title + ", writer=" + writer + ", content=" + content + ", no=" + no + "]";
+	}
+
+	
+	
+	
+	
+	
+	
+}
